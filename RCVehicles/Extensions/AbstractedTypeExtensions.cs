e0a520c61de6df5356bd64b87a7fff2bc27c32f6
Zereth <70236765+o5zereth@users.noreply.github.com>
2023-09-26 17:32:49 -0400

    Generics are better :)

--- RCVehicles/Extensions/AbstractedTypeExtensions.cs
+++ RCVehicles/Extensions/AbstractedTypeExtensions.cs
@@ -13,7 +13,6 @@
 namespace RCVehicles.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 public static class AbstractedTypeExtensions
@@ -22,81 +21,36 @@ public static class AbstractedTypeExtensions
     /// Every instance of the type found in any loaded assembly will be instantiated and returned into list form.
     /// </summary>
     /// <param name="type">The type to instantiate instances of.</param>
-    /// <returns>A list of all found instances of <see cref="type"/>.</returns>
-    public static List<object> InstantiateAllInstancesOfType(this Type type)
-    {
-        try
-        {
-
-            if (type == null)
-            {
-                return new List<object>();
-            }
-
-            List<object> instanceList = new List<object>();
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                try
-                {
-
-                    foreach (Type typeInstance in assembly.DefinedTypes)
-                    {
-                        if (typeInstance.IsAbstract || typeInstance.IsInterface)
-                            continue;
-
-                        if (typeInstance.BaseType != type)
-                        {
-                            continue;
-                        }
-
-                        object instance = Activator.CreateInstance(typeInstance);
-                        if (instance is null)
-                        {
-                            continue;
-                        }
-
-                        try
-                        {
-                            instance = Convert.ChangeType(instance, type);
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-
-                        instanceList.Add(instance);
-                    }
-                }
-                catch (Exception)
-                {
-                    // uh oh - just iterate past and ignore :trol:
-                }
-            }
-
-            return instanceList;
-        }
-        catch (Exception)
-        {
-            // this would be really bad but we can just catch and return empty to say null safe.
-            return new List<object>();
-        }
-    }
-
-    /// <summary>
-    /// Every instance of the type found in any loaded assembly will be instantiated and returned into list form.
-    /// </summary>
-    /// <typeparam name="T">The type to instantiate instances of.</typeparam>
     /// <returns>A list of all found instances of <see cref="T"/>.</returns>
     public static List<T> InstantiateAllInstancesOfType<T>()
+        where T : class
     {
-        try
+        Type type = typeof(T);
+        List<T> instanceList = [];
+
+        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
         {
-            List<T> nullSafe = (List<T>)InstantiateAllInstancesOfType(typeof(T)).Cast<List<T>>();
-            return nullSafe;
+            try
+            {
+                foreach (Type typeInstance in HarmonyLib.AccessTools.GetTypesFromAssembly(assembly))
+                {
+                    if (typeInstance.IsAbstract || typeInstance.IsInterface)
+                        continue;
+
+                    if (!typeInstance.IsSubclassOf(type))
+                        continue;
+
+                    if (Activator.CreateInstance(typeInstance) is not T instance)
+                        continue;
+
+                    instanceList.Add(instance);
+                }
+            }
+            catch
+            {
+            }
         }
-        catch (Exception)
-        {
-            return new List<T>();
-        }
+
+        return instanceList;
     }
 }
